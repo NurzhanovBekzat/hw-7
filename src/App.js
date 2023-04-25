0f6b3b4709d479096dc1a12a27d85d967a917999
Nurzhanov-Bekzat <bekzatnurzhanov888@gmail.com>
2023-04-25 18:07:58 +0600

    finished-hw-7

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import { NewExpence } from "./componets/newExpence/NewExpence";
+
+function App() {
+  return (
+    <div className="App">
+      <NewExpence />
+    </div>
+  );
+}
+export default App;
+
+
+
+
+
